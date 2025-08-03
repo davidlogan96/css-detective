09631b2fd9df912a107d4a686b4a38b7411a61f0
David Logan <davidlogan96@gmail.com>
2025-08-03 01:14:13 +0000

    Do you think we can make the arrow a bit more pretty? I'll leave it up t

--- src/components/quiz.tsx
+++ src/components/quiz.tsx
@@ -138,7 +138,9 @@ export default function Quiz() {
               <div
                 style={currentQuestion.originalStyle}
               />
-              <ArrowRight className="h-8 w-8 text-foreground/30 shrink-0" />
+               <div className="flex items-center justify-center rounded-full border border-dashed p-2 bg-background/20">
+                <ArrowRight className="h-6 w-6 text-foreground/50 shrink-0" />
+              </div>
               <div
                 className="flex items-center justify-center transition-all duration-300 ease-in-out"
                 style={currentQuestion.style}
